f89821ed0a82fc54c170ed16f1e3474c650f1697
Sanya Oluwadare <sanyaodare@gmail.com>
2021-12-10 15:02:01 +0100

    set up server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+
+app.listen (3000, (req, res) => {
+  console.log('Server is running on port 3000....');  
+});
