32dfbddac75868016f118cf4c98a4192d78b4fef
Sanya Oluwadare <sanyaodare@gmail.com>
2021-12-12 20:33:45 +0100

    get mailchimp api key

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', (req, res) => {
+
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+
+  console.log(firstName, lastName, email);
+  
+})
+
 app.listen (3000, (req, res) => {
   console.log('Server is running on port 3000....');  
 });
+
+
+// 47a42095679b10a3b2d6c0b4be2d8f2a-us20
